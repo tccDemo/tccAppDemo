f104000d51722b6f22a0a53d8b752642aa951945
Kevin Xu <kxu@campuscruiser.com>
2017-03-02 17:27:06 +0800

    announcement style

--- src/providers/announcement.ts
+++ src/providers/announcement.ts
@@ -2,7 +2,6 @@ export class Announcement {
   id: number;
   title: string;
   detail: string;
-  icon: string;
   postedBy: string;
   postedAt: string;
   postedFrom: string;
--- src/providers/mock-announcement.ts
+++ src/providers/mock-announcement.ts
@@ -1,11 +1,21 @@
 import { Announcement } from './announcement';
 
 export const ANNOUNCEMENTS: Announcement[] = [
-  {id: 301, title: 'Announcement 1', postedFrom: 'Class 1', detail: "Hello World Hello World ", icon:"", postedBy:"Peter", postedAt: "Today"},
-  {id: 302, title: 'Announcement 2', postedFrom: 'Class 5', detail: "Hello World Hello World ", icon:"", postedBy:"Linda", postedAt: "Today"},
-  {id: 303, title: 'Announcement 3', postedFrom: 'Office 1', detail: "Hello World Hello World ", icon:"", postedBy:"Marry", postedAt: "Dec 19, 2016"},
-  {id: 304, title: 'Announcement 4', postedFrom: 'Club 1', detail: "Hello World Hello World ", icon:"", postedBy:"John", postedAt: "Nov 10, 2016"},
-  {id: 305, title: 'Announcement 5', postedFrom: 'Club 3', detail: "Hello World Hello World ", icon:"", postedBy:"Peter", postedAt: "Aug 8, 2016"},
+  {id: 301, title: 'Announcement 1', postedFrom: 'Class 1', detail: "Hello World Hello World ", postedBy:"Peter", postedAt: "Today"},
+  {id: 302, title: 'Announcement 2', postedFrom: 'Class 5', detail: "Hello World Hello World ", postedBy:"Linda", postedAt: "Today"},
+  {id: 303, title: 'Announcement 3', postedFrom: 'Office 1', detail: "Hello World Hello World ", postedBy:"Marry", postedAt: "Dec 19, 2016"},
+  {id: 304, title: 'Announcement 4', postedFrom: 'Club 1', detail: "Hello World Hello World ", postedBy:"John", postedAt: "Nov 10, 2016"},
+  {id: 305, title: 'Announcement 5', postedFrom: 'Club 3', detail: "Hello World Hello World ", postedBy:"Peter", postedAt: "Aug 8, 2016"},
+  {id: 306, title: 'Announcement 6', postedFrom: 'Class 1', detail: "Hello World Hello World ", postedBy:"Peter", postedAt: "Today"},
+  {id: 307, title: 'Announcement 7', postedFrom: 'Class 5', detail: "Hello World Hello World ", postedBy:"Linda", postedAt: "Today"},
+  {id: 308, title: 'Announcement 8', postedFrom: 'Office 1', detail: "Hello World Hello World ", postedBy:"Marry", postedAt: "Dec 19, 2016"},
+  {id: 309, title: 'Announcement 9', postedFrom: 'Club 1', detail: "Hello World Hello World ", postedBy:"John", postedAt: "Nov 10, 2016"},
+  {id: 310, title: 'Announcement 10', postedFrom: 'Club 3', detail: "Hello World Hello World ", postedBy:"Peter", postedAt: "Aug 8, 2016"},
+  {id: 311, title: 'Announcement 11', postedFrom: 'Class 1', detail: "Hello World Hello World ", postedBy:"Peter", postedAt: "Today"},
+  {id: 312, title: 'Announcement 12', postedFrom: 'Class 5', detail: "Hello World Hello World ", postedBy:"Linda", postedAt: "Today"},
+  {id: 313, title: 'Announcement 13', postedFrom: 'Office 1', detail: "Hello World Hello World ", postedBy:"Marry", postedAt: "Dec 19, 2016"},
+  {id: 314, title: 'Announcement 14', postedFrom: 'Club 1', detail: "Hello World Hello World ", postedBy:"John", postedAt: "Nov 10, 2016"},
+  {id: 315, title: 'Announcement 15', postedFrom: 'Club 3', detail: "Hello World Hello World ", postedBy:"Peter", postedAt: "Aug 8, 2016"},
 
 ]  
   

454461ffe167fe009489e3b258b27237d7bc6a1e
Fred <fwang@campuscruiser.com>
2017-03-04 23:27:40 +0800

    adjust Login page( Progressing )

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,10 +10,10 @@ import { LoginPage } from '../pages/login/login';
   templateUrl: 'app.html'
 })
 export class MyApp {
-  rootPage = FooterPage;
+  rootPage = LoginPage;
 
-  constructor(plt: Platform) {
-    plt.ready().then(() => {
+  constructor(platform: Platform) {
+    platform.ready().then(() => {
       // Okay, so the platform is ready and our plugins are available.
       // Here you can do any higher level native things you might need.
       StatusBar.styleDefault();

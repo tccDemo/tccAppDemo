44b4ff8973c43235ae9962c9c328c016d5155b7e
徐子平 <kxu@xuzipingdeMac-mini.local>
2017-06-11 22:37:00 +0800

    update

--- api/apiconf.js
+++
@@ -1,8 +0,0 @@
-window.apiTesterConfig = {
-  installId : "98hD-SDXs-8sq1-NTH7-sdGS-566h",
-  campusId : 30018,
-  productName: 'Cathay',
-  ccapi_server_url : "http://ximage.mooxedu.com",
-  user_account : "studentqa",
-  user_password : "000000"
-};
--- api/swagger/TCCRest.sh
+++
@@ -1,5 +0,0 @@
-java -jar swagger-codegen-cli.jar generate \
-  -i swagger.json \
-  -l jaxrs \
-  -o tccswagger \
-  -c TCCRestConf.json
